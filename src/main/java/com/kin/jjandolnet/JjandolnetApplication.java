91fd9bd802c30c9cba38e3abb28f1092e1db8c7e
kin412 <kiwo412@gmail.com>
2026-04-18 16:12:06 +0900

    create project

---
+++ src/main/java/com/kin/jjandolnet/JjandolnetApplication.java
@@ -0,0 +1,13 @@
+package com.kin.jjandolnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjandolnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjandolnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kin/jjandolnet/JjandolnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kin.jjandolnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjandolnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
