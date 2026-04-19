e990a50d1954da3e70bf97267d6495131914e06a
kin412 <kiwo412@gmail.com>
2026-04-19 17:34:42 +0900

    BaseTimeEntity audit

---
+++ src/main/java/com/kin/jjandolnet/global/common/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.kin.jjandolnet.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate // 생성 시 자동 저장
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
